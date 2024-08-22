c31a1beb2266f360d5ca5c363923a0b441337138
Edmar Batista <edmarbpaiva@gmail.com>
2024-08-22 06:07:55 -0300

    terceiro commit

--- Hooks/src/components/UseCallback.jsx
+++ Hooks/src/components/UseCallback.jsx
@@ -1,42 +1,59 @@
-import React, { useState, useEffect, useMemo } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 
-export default function () {
 
-    const [valor, setValor] = useState(() => { return 0 })
-    const [numero, setNumero] = useState(() => { return 1000 })
-
-    // const valorCalculado = funcaoDemorada(valor)   // Se eu usar essa função vai demorar a executar
-    const valorCalculado = useMemo(() => {
-        return funcaoDemorada(valor)
-    }, [valor]) // A função só vai ser recalculada se eu alterar valor,   Se eu alterar numero a função não vai ser executada
-
+function Lista({ resultados }) {
+    const [listaItems, setListaItems] = useState([])
     useEffect(() => {
-        console.log('Renderizado (useMemo)')
-    }) // Para perceber quando nossa aplicação está sendo renderizada
+        setListaItems(resultados())
+        console.log('RENDER')
+    }, [resultados])
+    return (
+        listaItems.map(item => <p key={item}>{item}</p>)
+    )
+}
 
 
 
-    function incrementar() {
-        setValor(oldValor => oldValor + 1)
+
+
+
+export default function App() {
+    const [numero, setNumero] = useState(100)
+    const [temaEscuro, setTemaEscuro] = useState(false)
+    // Função para calcular lista de resultados
+
+    // ********* Com a função assim o componente lista sempre será rederizado, mesmo que eu somente altere o tema para claro ou escuro
+    // const calculos = () => { 
+    //     return [numero * 2, numero * 3, numero * 4]
+    // }
+
+    // Agora quando eu altero o tema não ocorre uma renderização da lista devido à execução da função calculos
+    const calculos = useCallback(() => {
+        return [numero * 2, numero * 3, numero * 4]
+    }, [numero])
+
+
+
+
+    // tema da app
+    const tema = {
+        backgroundColor: temaEscuro ? "black" : "white",
+        color: temaEscuro ? "white" : "black",
+        height: "50vh"
     }
-    function incrementar2() {
-        setNumero(oldNumero => oldNumero + 100)
-    }
 
-    function funcaoDemorada(num) {
-        for (let i = 0; i < 5000000000; i++) { } //Deixa a função mas lenta para simular um cálculo demorado
-        return num * 2
-    }
 
     return (
-        <div className="componente">
-            <h1>useMemo - USEMEMO</h1>
+        <div style={tema}>
+            <h1>REACT — useCallback</h1>
             <hr />
-            <p>Valor: {valor}</p>
-            <p>Número: {numero}</p>
-            <p>Valor calculado: {valorCalculado}</p>
-            <button onClick={incrementar}>Incrementar</button>
-            <button onClick={incrementar2}>Incrementar (useMemo)</button>
-        </div>
+            <button onClick={() => setTemaEscuro(oldTemaEscuro => !oldTemaEscuro)}>Alterar Tema</ button>
+            <p style={{ margin: "20px" }}>Número: {numero}</p>
+            <button onClick={() => setNumero(oldNumero => oldNumero + 1)}>Incrementar</button>
+            <hr />
+            <Lista resultados={calculos} />  {/* Calculos não é um valor, mas sim uma função */}
+        </div >
+
     )
+
 }
