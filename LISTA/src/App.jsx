7f1531972c49a3e49b562a8601aa0c1be37d1e5a
Edmar Batista <edmarbpaiva@gmail.com>
2024-08-23 07:28:55 -0300

    LISTA

---
+++ LISTA/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import './App.css'
+
+// componentes
+
+
+import Varios from "./components/Varios"
+
+
+
+//import HookRef16 from './components/UseRef_16'
+//import HookEffect17 from './components/UseEffect_17'
+
+
+function App() {
+    return (
+        <>
+            <Varios />
+
+        </>
+    )
+}
+
+export default App
