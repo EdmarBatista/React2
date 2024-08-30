3fecc0e47375988c95d7b50e9b23b622b175acad
Edmar Batista <edmarbpaiva@gmail.com>
2024-08-30 08:37:17 -0300

    LISTA - Bootstrap

--- LISTA/src/App.jsx
+++ LISTA/src/App.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useRef, useEffect } from "react"
 import ListaContactos from "./components/ListaContacto"
 import Contacto from "./components/Contacto"
-
+import './App.css'
 import { v4 as chave } from 'uuid'
+
+
+
 function App() {
 
     // states
@@ -90,25 +93,72 @@ function App() {
 
     return (
         <>
-            <h1>Minha lista de contatos</h1>
-            <hr />
-            <div>
-                <label >Nome:<br /></label>
-                <input type="text" ref={inputNome} onChange={definirNome} value={contacto.nome} />
+            {/* Importação do Bootstrap */}
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" ></script>
+
+
+            <div className="cantainer-fluid titulo">
+                <div className="row">
+                    <div className="col text-center">
+                        <h4 className="text-center">LISTA DE CONTATOS</h4>
+                    </div>
+                </div>
             </div>
-            <div>
-                <label >Telefone:<br /></label>
-                <input type="text" ref={inputTelefone} onChange={definirTelefone} onKeyUp={enterAdicionarContacto} value={contacto.telefone} />
+
+
+
+
+            <div className="container-fluid formulario">
+                <div className="row">
+                    <div className="col p-3">
+
+
+
+                        <div className="row justify-content-center">
+                            <div className="col-10 col-sm-8 cls-md-6 col-lg-4">
+
+
+                                <div className="mb-3">
+                                    <label className="form-label">Nome</label><br />
+                                    <input type="text" ref={inputNome} onChange={definirNome} value={contacto.nome} className="form-control" />
+                                </div>
+                                <div>
+                                    <label className="form-label">Telefone</label><br />
+                                    <input type="text" ref={inputTelefone} onChange={definirTelefone} onKeyUp={enterAdicionarContacto} value={contacto.telefone} className="form-control" />
+                                </div>
+
+
+                                <div className="row mt-3 ">
+                                    <div className="col text-start">
+                                        <button onClick={limparStorage} className="btn btn-outline-warning">Limpar Lista</button>
+                                    </div>
+                                    <div className="col text-end">
+                                        <button onClick={adicionarContato}  className="btn btn-outline-info">Adicionar Concacto</button>
+                                    </div>
+                                </div>
+
+
+
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                </div>
             </div>
-            <button onClick={adicionarContato}>Adicionar Concacto</button>
-            <button onClick={limparStorage}>Limpar Lista</button>
-            <hr />
+
+
+
+
+
             {/* <ListaContactos listaContactos={listaContactos} /> */}
             {/* Apresentação da lista de contactos  */}
             {listaContactos.map(ct => {
 
                 // return <Contacto key={chave()} nome={ct.nome} telefone={ct.telefone} remover={removerContacto} />   // Usado quando eu removia os elemntos da lista por meio da nome  e telefone
-                return <Contacto key={ct.id} id = {ct.id} nome={ct.nome} telefone={ct.telefone} remover={removerContacto} />
+                return <Contacto key={ct.id} id={ct.id} nome={ct.nome} telefone={ct.telefone} remover={removerContacto} />
 
 
             })}
